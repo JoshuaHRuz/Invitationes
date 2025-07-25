9794cc7fd7295506821bf5c37adc8e91d7b0d127
JoshuaHRuz <columbia2000@live.com.mx>
2025-07-24 23:36:50 -0600

    Story component

--- Frontend/digital-invitations-frontend/src/app/features/invitations/components/story/story.component.ts
+++ Frontend/digital-invitations-frontend/src/app/features/invitations/components/story/story.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,44 @@ import { CommonModule } from '@angular/common';
   templateUrl: './story.component.html',
   styleUrls: ['./story.component.scss']
 })
-export class StoryComponent {
+export class StoryComponent implements OnInit, OnDestroy {
+  children = [
+    { name: 'Luis Manuel Hernández', photo: 'assets/images/photos/son-1.png' },
+    { name: 'Paola Horta Rojas', photo: 'assets/images/photos/son-2.png' },
+    { name: 'José Alberto Vallejo Aguilar', photo: 'assets/images/photos/son-3.png' }
+  ];
 
+  currentChildIndex = 0;
+  currentChildTransform = 0;
+  private intervalId: any;
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startCarousel() {
+    this.intervalId = setInterval(() => {
+      this.nextChild();
+    }, 5000); // Cambia cada 5 segundos
+  }
+
+  nextChild() {
+    this.currentChildIndex = (this.currentChildIndex + 1) % this.children.length;
+    this.updateTransform();
+  }
+
+  prevChild() {
+    this.currentChildIndex = (this.currentChildIndex - 1 + this.children.length) % this.children.length;
+    this.updateTransform();
+  }
+
+  updateTransform() {
+    this.currentChildTransform = -this.currentChildIndex * 100;
+  }
 } 
