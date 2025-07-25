2616f2f140d29b6b6f9c01a09ddb0df01620ba96
JoshuaHRuz <columbia2000@live.com.mx>
2025-07-24 20:48:03 -0600

    Invitation template component

---
+++ Frontend/digital-invitations-frontend/src/app/features/invitations/invitations.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TemplateComponent } from './template/template.component';
+
+export const INVITATIONS_ROUTES: Routes = [
+    { path: '', component: TemplateComponent }
+]; 
