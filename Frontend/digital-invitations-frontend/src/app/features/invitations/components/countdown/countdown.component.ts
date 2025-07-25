40be1d3bcf1bc0a3063767b2f616a256abbd699c
JoshuaHRuz <columbia2000@live.com.mx>
2025-07-24 23:48:58 -0600

    Countdown component

--- Frontend/digital-invitations-frontend/src/app/features/invitations/components/countdown/countdown.component.ts
+++ Frontend/digital-invitations-frontend/src/app/features/invitations/components/countdown/countdown.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,42 @@ import { CommonModule } from '@angular/common';
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit, OnDestroy {
+  private intervalId: any;
+  private targetDate = new Date('2026-02-04T00:00:00');
 
+  public days: number = 0;
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+
+  ngOnInit() {
+    this.startCountdown();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startCountdown() {
+    this.intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = this.targetDate.getTime() - now;
+
+      if (distance < 0) {
+        clearInterval(this.intervalId);
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+      } else {
+        this.days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      }
+    }, 1000);
+  }
 } 
