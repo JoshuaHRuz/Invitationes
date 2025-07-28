b0a3429fe4a89090196453f38b2d388bc40d6f03
JoshuaHRuz <columbia2000@live.com.mx>
2025-07-27 18:37:29 -0600

    Itinerary and rsvp components

--- Frontend/digital-invitations-frontend/src/app/features/invitations/components/itinerary/itinerary.component.ts
+++ Frontend/digital-invitations-frontend/src/app/features/invitations/components/itinerary/itinerary.component.ts
@@ -9,5 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./itinerary.component.scss']
 })
 export class ItineraryComponent {
-
+  timeline = [
+    { time: '12:00', title: 'Ceremonia Religiosa', icon: '/assets/icons/church.png' },
+    { time: '15:30', title: 'Recepción y Cóctel', icon: '/assets/icons/drink.png' },
+    { time: '16:00', title: 'Sesión de Fotos', icon: '/assets/icons/camera.png' },
+    { time: '17:00', title: 'Comida', icon: '/assets/icons/comida.png' },
+    { time: '18:00', title: '¡A bailar!', icon: '/assets/icons/music-dance.png' },
+    { time: '20:00', title: 'Brindis', icon: '/assets/icons/glass.png' },
+    { time: '20:15', title: 'Baile de los Novios', icon: '/assets/icons/dance.png' },
+    { time: '21:00', title: 'Corte de Pastel', icon: '/assets/icons/cake.png' },
+    { time: '21:20', title: '¡Disfruta la Fiesta!', icon: '/assets/icons/music-dance.png' }
+  ];
 } 
--- Frontend/digital-invitations-frontend/src/app/features/invitations/components/rsvp-form/rsvp-form.component.ts
+++ Frontend/digital-invitations-frontend/src/app/features/invitations/components/rsvp-form/rsvp-form.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormArray, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rsvp-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './rsvp-form.component.html',
   styleUrls: ['./rsvp-form.component.scss']
 })
-export class RsvpFormComponent {
+export class RsvpFormComponent implements OnInit {
+  // Simulación de datos de la invitación
+  invitationTo = "Familia Hernández";
+  predefinedGuests = [
+    { name: 'Sarai Flores', isEditable: false },
+    { name: 'Mauricio Garcia', isEditable: false },
+    { name: 'Acompañante 3', isEditable: true },
+    { name: 'Acompañante 4', isEditable: true }
+  ];
 
+  rsvpForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.rsvpForm = this.fb.group({
+      guests: this.fb.array([]),
+      message: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.initializeGuestForms();
+  }
+
+  get guests(): FormArray {
+    return this.rsvpForm.get('guests') as FormArray;
+  }
+
+  initializeGuestForms() {
+    this.predefinedGuests.forEach(guest => {
+      this.guests.push(this.fb.group({
+        name: [guest.name],
+        isEditable: [guest.isEditable],
+        attendance: ['attending'] // 'attending' o 'not_attending'
+      }));
+    });
+  }
+
+  onSubmit() {
+    console.log('Confirmación enviada:', this.rsvpForm.value);
+    alert('¡Gracias por tu respuesta!');
+  }
 } 
